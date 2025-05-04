9aedfa8098e9afec5d1d85a12705e44ff5473b48
Shaheen Naiyer <snshohin@gmail.com>
2025-05-04 16:55:11 +0530

    navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,13 @@ import Reactt, {useState} from "react"
 import {getImageUrl} from "../../utils";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt='menu-btn'/>
-            <ul className={styles.menuItems}>
+            <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt='menu-btn' onClick={()=> setMenuOpen(!menuOpen)}/>
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={()=> setMenuOpen(false)}>
                 <li>
                     <a href="#about">About</a>
                 </li>
